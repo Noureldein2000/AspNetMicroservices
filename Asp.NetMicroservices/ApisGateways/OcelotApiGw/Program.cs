d27fcdbb040043e86ac440a4f67296a29a2c5dc0
Nour <noureldin@tafeel.com>
2025-09-24 16:18:24 +0300

    implement  Ocelot Api gateway

---
+++ Asp.NetMicroservices/ApisGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,20 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// ✅ إضافة تسجيل الدخول (Logging)
+builder.Logging.ClearProviders(); // اختياري لمسح الإعدادات الافتراضية إن رغبت
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
+
+app.Run();
