b38dbdc22a89950619cded7bf6da1d7b8ac371a0
Noureldein Hussein <56475893+Noureldein2000@users.noreply.github.com>
2025-09-30 08:30:04 +0300

    modified and dockerieze contanerz for ocelot services apigateway

--- Asp.NetMicroservices/ApisGateways/OcelotApiGw/Program.cs
+++ Asp.NetMicroservices/ApisGateways/OcelotApiGw/Program.cs
@@ -1,13 +1,22 @@
 ﻿using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
+;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// ✅ إضافة ملفات إعدادات Ocelot
+ //✅ إضافة ملفات إعدادات Ocelot
 builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
 {
     config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: false, reloadOnChange: true);
 });
+
+// Ocelot Basic setup
+//builder.Configuration
+//    .SetBasePath(builder.Environment.ContentRootPath)
+//    .AddOcelot(); // single ocelot.json file in read-only mode
+//builder.Services
+//    .AddOcelot(builder.Configuration);
 
 // ✅ إضافة تسجيل الدخول (Logging)
 builder.Logging.ClearProviders(); // اختياري لمسح الإعدادات الافتراضية إن رغبت
@@ -15,12 +24,13 @@ builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot(builder.Configuration)
+      .AddCacheManager(x => x.WithDictionaryHandle());
 
 var app = builder.Build();
 
-app.MapGet("/hello", () => "Hello World from Downstream!");
+app.MapGet("/", () => "Hello World from Downstream!");
 
 await app.UseOcelot();
 
-app.Run("http://localhost:5010");
+app.Run();
